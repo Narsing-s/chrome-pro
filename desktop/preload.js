4ef772bff4f5fe91e8a55ed92980cbb7a03dec00
Narsingarao Beesetti <narsingbeesetti006@gmail.com>
2026-08-30 21:13:39 +0530

    feat: expose browser profile and tab controls

--- desktop/preload.js
+++ desktop/preload.js
@@ -1,8 +1,12 @@
 const { contextBridge, ipcRenderer } = require('electron');
 
 contextBridge.exposeInMainWorld('chromePro', {
-  version: '0.2.0',
+  version: '0.3.0',
   getAppInfo: () => ipcRenderer.invoke('app:info'),
-  openExternal: (url) => ipcRenderer.invoke('browser:external', url),
-  clearSiteData: () => ipcRenderer.invoke('browser:clear-data')
+  open: (url) => ipcRenderer.invoke('browser:open', url),
+  newProfile: (name) => ipcRenderer.invoke('browser:new-profile', name),
+  getHistory: () => ipcRenderer.invoke('browser:history'),
+  getTabs: () => ipcRenderer.invoke('browser:tabs'),
+  clearSiteData: () => ipcRenderer.invoke('browser:clear-data'),
+  openExternal: (url) => ipcRenderer.invoke('browser:external', url)
 });
