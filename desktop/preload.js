0268c9c903c5ffcda84fd133b041edf8048c325d
Narsingarao Beesetti <narsingbeesetti006@gmail.com>
2026-08-30 21:14:44 +0530

    feat: expose full browser controls to UI

--- desktop/preload.js
+++ desktop/preload.js
@@ -1,12 +1,9 @@
 const { contextBridge, ipcRenderer } = require('electron');
-
 contextBridge.exposeInMainWorld('chromePro', {
-  version: '0.3.0',
-  getAppInfo: () => ipcRenderer.invoke('app:info'),
-  open: (url) => ipcRenderer.invoke('browser:open', url),
-  newProfile: (name) => ipcRenderer.invoke('browser:new-profile', name),
-  getHistory: () => ipcRenderer.invoke('browser:history'),
-  getTabs: () => ipcRenderer.invoke('browser:tabs'),
-  clearSiteData: () => ipcRenderer.invoke('browser:clear-data'),
-  openExternal: (url) => ipcRenderer.invoke('browser:external', url)
+  version:'0.4.0', getAppInfo:()=>ipcRenderer.invoke('app:info'), open:u=>ipcRenderer.invoke('browser:open',u),
+  newTab:()=>ipcRenderer.invoke('browser:new-tab'), closeTab:i=>ipcRenderer.invoke('browser:close-tab',i), switchTab:i=>ipcRenderer.invoke('browser:switch-tab',i),
+  newProfile:n=>ipcRenderer.invoke('browser:new-profile',n), incognito:()=>ipcRenderer.invoke('browser:incognito'), tabs:()=>ipcRenderer.invoke('browser:tabs'),
+  history:()=>ipcRenderer.invoke('browser:history'), bookmarks:()=>ipcRenderer.invoke('browser:bookmarks'), addBookmark:b=>ipcRenderer.invoke('browser:add-bookmark',b),
+  removeBookmark:u=>ipcRenderer.invoke('browser:remove-bookmark',u), downloads:()=>ipcRenderer.invoke('browser:downloads'), clearSiteData:()=>ipcRenderer.invoke('browser:clear-data'),
+  openExternal:u=>ipcRenderer.invoke('browser:external',u)
 });
